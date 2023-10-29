e53872a912bd113f9af9fd45cdf305490415676b
joel chacon castillo <joel.chacon@cimat.mx>
2023-10-29 15:47:36 -0600

    ..

--- 906_Div2/C/main.cpp
+++ 906_Div2/C/main.cpp
@@ -1,3 +1,11 @@
+/**
+  insert 01
+  a[i] != a[k-i+1] forall i=1,..,k
+  0------0+01
+  01+1------1
+  Base case:
+     N is odd: middle item is always the same..
+ * */
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
@@ -9,22 +17,31 @@ int main(){
       int n;
       string s;
       cin>>n>>s;
-      vector<int> op;
-      int limit=0, i = 0;      
-      while(i < s.size() && limit < 300){
-	 if(s[i]==s[s.size()-i-1]){
-	   op.push_back(s.size()-i);
-	   s.insert(s.size()-i, "01");
-	   limit++;
-	 }else i++;
+      if(n%2==1){
+	     cout <<-1<<"\n";
+	     continue;
       }
-      if(limit==300){
+      vector<int> op;
+      int cs=s.size(), idx=0; 
+      while( !s.empty() && op.size() <= 300){
+	   if(s.front()==s.back()){
+	     //if(s.front()=='0') op.push_back(s.size()+idx),s=s+"01";
+	     if(s.front()=='0') op.push_back(cs-idx),s=s+"01";
+	     else op.push_back(idx), s = "01"+s;
+	     cs+=2;
+	   }
+	   s = s.substr(1, s.size()-2);
+	   idx++;
+      }
+      if(op.size() > 300){
 	      cout <<-1<<endl;
 	      continue;
       }
       cout << op.size()<<"\n";
+      if(!op.empty()){
       for(auto i:op)cout << i<<" ";
       cout<<endl;
+      }
    }
    return 0;
 }
