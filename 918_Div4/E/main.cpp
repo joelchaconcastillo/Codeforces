8aba2a7e2f6becbda162a8712fca987fd1c7d7ab
joel chacon castillo <joelchaconcastillo@gmail.com>
2024-01-28 23:22:37 -0600

    ..

--- 918_Div4/E/main.cpp
+++ 918_Div4/E/main.cpp
@@ -8,18 +8,22 @@ int main(){
    while(T--){
       int n;
       cin>>n;
-      vector<long long>a(n), b(n)
+      vector<long long>a(n);
       for(int i = 0 ; i < n ; i++) cin>>a[i];
-      long long sumodd=0, sumeven=0;
-      for(int i = 0 ;i < n ; i++){
-	 if(i%2==0)sumeven+=a[i];
-	 else sumodd+=a[i];
-	 b[i] = sumodd-sumeven;
-      }
+      long long sum=0;
+      map<long long, long long> count;
+      bool hasSol=false;
+      count[0]=1;
       for(int i = 0 ; i < n; i++){
-	 
+         sum += a[i]*(  (i%2==0)?1LL:-1LL );
+         if(count[sum]){
+            hasSol=true;
+            break;
+         }
+         count[sum]++;
       }
-
+      if(hasSol) cout << "YES\n";
+      else cout << "NO\n";
    }
    return 0;
 }
--- 918_Div4/F/main.cpp
+++ 918_Div4/F/main.cpp
@@ -1,30 +1,28 @@
 #include <bits/stdc++.h>
-using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std; 
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+
 int main(){
    ios::sync_with_stdio(0);
    cin.tie(0);
    int T;
    cin>>T;
    while(T--){
-      int n;
+      int n; 
       cin>>n;
-      vector<pair<long long, long long>>ab(n);
-      for(int i = 0 ; i < n ; i++){
-	  cin>>ab[i].second>>ab[i].first;
-	  ab[i].first = -ab[i].first;
-	  ab[i].second = -ab[i].second;
+      vector<pair<int, int>>a(n);
+      for(int i = 0 ; i < n ; i++)  cin>>a[i].second>>a[i].first;
+      sort(a.begin(), a.end());
+      long long counter = 0;
+      ordered_set as;
+      for(int i = 0 ; i < n; i++){
+         counter += as.size() - as.order_of_key(a[i].second);
+         as.insert(a[i].second);
       }
-      sort(ab.begin(), ab.end());
-      long long t0 = ab.front().first, res=0;
-      priority_queue<long long>pq;
-      for(int i = 0 ; i< n ; i++){
-	 long long elapsed = ab[i].first - t0;
-	 ab[i].first= min(ab[i].first + elapsed, ab[i].second);
-	 while(!pq.empty() && -pq.top() < ab[i].first) pq.pop();
-	 res += pq.size();
-	 pq.push(-ab[i].second);
-      }
-      cout << res <<endl;
+      cout << counter<<endl;
    }
    return 0;
 }
